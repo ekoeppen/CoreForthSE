14e65e708f3c6ca5194acad40c786fb2a1c91a3a
Eckhart Koeppen <eck@40hz.org>
2016-05-09 15:55:54 +0300

    Fix DOES so that it uses an offset from the code field start to patch the current word

--- generic/CoreForthSE.s
+++ generic/CoreForthSE.s
@@ -1952,7 +1952,7 @@ is_positive:
     defword "(DOES>)", XDOES
     bl HERE
     pop {r1}; subs r1, #1; ppush r1
-    lit8 -8; bl ALLOT;
+    bl LATEST; bl FETCH; bl FROMLINK; lit8 10; bl ADD; bl ORG
     lit32 0xb500; bl COMMAH
     bl COMMAXT;
     bl ORG
